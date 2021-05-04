84d69df365d41842fc1151ffd03c2504e2d5f4a7
erik torkelsson <erik.torkelsson@hotmail.com>
2021-05-04 18:56:46 +0200

    tyuityiu

--- brokerpage/src/App.js
+++ brokerpage/src/App.js
@@ -1,6 +1,13 @@
 
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+
 function App() {
   return (
     <div className="App">
